3b43a6f2910f15b646c2304571b3ec9294239477
Putut Singgih Pamungkas <putut.singgihpamungkas@gmail.com>
2025-01-26 22:12:43 +0700

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -27,9 +27,3 @@ favicon.rel = "icon";
 favicon.href = "/images/title2.png"; // Ganti dengan path logo Anda
 document.head.appendChild(favicon);
 
-
-document.querySelector('.dropdown-btn').addEventListener('click', function (e) {
-  e.preventDefault(); // Cegah aksi default (jika button memiliki tautan)
-  const dropdownContent = document.querySelector('.dropdown-content');
-  dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-});
